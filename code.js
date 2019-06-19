6143b890f9293f5d4be47e9681754bfd326d9b61
Fantasy <jie844067636@gmail.com>
2019-06-19 13:50:03 +0800

    Update code.js

--- code.js
+++ code.js
@@ -1,15 +1,5 @@
-import { time } from 'universal'
-import { Man } from 'Earth'
-const me = new Man()
-
-while (time && me.alive()) {
-  const now = new Date()
-  if (!now.isWorkday()) me.play()
-  switch (now) {
-    case '09:00 AM': me.work();  break
-    case '12:00 AM': me.eat();   break
-    case '01:30 PM': me.work();  break
-    case '06:00 PM': me.play();  break
-    default:         me.sleep(); break
+function checkError (error) {
+  if (error) {
+    // doNothing
   }
 }
